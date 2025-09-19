f0397cc5808aace8d00ccae350cca0f0bc8cb1ff
John Kananda <jkananda5@outlook.com>
2025-09-18 23:40:29 -0600

    esLint Correction

--- app/public/chairman-announcements/page.tsx
+++ app/public/chairman-announcements/page.tsx
@@ -3,6 +3,34 @@ export default function ChairmanAnnouncementsPage() {
     <div className="space-y-4 max-w-3xl mx-auto">
       <h2 className="text-2xl font-bold text-green-800">Chairman’s Announcements</h2>
       <p>Bereavements and official messages from the Chairman will be posted here.</p>
+Hello Kenyan Family. 
+
+Our community thrives when we invest in the next generation - our Youth. 
+
+Communities from other Countries are  doing very well in mentoring and guiding their youth to be the future leaders of this Country.
+
+We can do the same for our youth. 
+
+Very often, children don't take the words of advise from their parents, but  they quite often heed the words of counsel from someone else.
+
+Let us begin the journey of guiding our youth, as a Community so that we can lay the foundations of their future sucess.
+
+In this regard, we are organizing a Kenyan Youth Meet-Up Event, where they can have fun together, and we can then engage them in discussions - about career planning, mental health, drugs and addictions, substance abuse, being financially smart, the importance of belonging to a successful community, among other relevant topics.
+
+We can also get some of our youth, to assist in arranging for the event, so that they have ownership.
+
+As well, we can have some of our young adults talk to the youth as well, so they can see themselves in the ones talking to them. 
+
+The date of the event is set for the last Saturday of this month, on AUGUST 30th for ALL Kenyan youth in their early teens to mid twenties (Around 13 - 25 years old). 
+
+Please Reserve the date for your Children. 
+
+We thank the KCA Leadership for being a part of this initiative. 
+
+More details to follow.
+
+Thank you so much  🙏🏾
+
     </div>
   )
 }
